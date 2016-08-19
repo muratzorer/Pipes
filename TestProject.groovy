cc7c35c5b164f6bbf8bad5d2f04d68f7064df730
muratzorer <muratzorer35@gmail.com>
2016-08-20 01:28:59 +0300

    return this added

--- TestProject.groovy
+++ TestProject.groovy
@@ -1,5 +1,5 @@
 def nunitStep() {
-	println "Hello, world!"
+	return "nunit3-console TestApplication.Tests\\bin\\Release\\TestApplication.Tests.dll --result:nunit-result.xml;format=nunit2"
 }
 
 return this;
