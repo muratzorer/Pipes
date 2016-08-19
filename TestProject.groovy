7708e05544363fe0e6e5ceb1a0a97bcd8089994c
muratzorer <muratzorer35@gmail.com>
2016-08-20 01:10:39 +0300

    string return added

--- TestProject.groovy
+++ TestProject.groovy
@@ -1,5 +1,3 @@
 def nunitStep() {
-	bat 'nunit3-console TestApplication.Tests\\bin\\Release\\TestApplication.Tests.dll --result:nunit-result.xml;format=nunit2'
+	return 'nunit3-console TestApplication.Tests\\bin\\Release\\TestApplication.Tests.dll --result:nunit-result.xml;format=nunit2'
 }
-
-return this;
