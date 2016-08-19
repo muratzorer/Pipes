cdec919a853fd78e8d7bb0d9f3cae6b2493019e6
muratzorer <muratzorer35@gmail.com>
2016-08-20 01:20:39 +0300

    updated return

--- TestProject.groovy
+++ TestProject.groovy
@@ -1,3 +1,3 @@
 def nunitStep() {
-	return 'nunit3-console TestApplication.Tests\\bin\\Release\\TestApplication.Tests.dll --result:nunit-result.xml;format=nunit2'
+	return "nunit3-console TestApplication.Tests\\bin\\Release\\TestApplication.Tests.dll --result:nunit-result.xml;format=nunit2"
 }
